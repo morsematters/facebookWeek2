1b927e0946383c862df7ca13514d46b77a1fee48
morsematters <ericamorse@ericamorse-macbookpro2.roam.corp.google.com>
2014-06-25 15:54:44 -0700

    Initial Commit

---
+++ facebookWeek2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  facebookWeek2
+//
+//  Created by Erica Morse on 6/25/14.
+//  Copyright (c) 2014 YouTube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ facebookWeek2Tests/facebookWeek2Tests.m
@@ -0,0 +1,34 @@
+//
+//  facebookWeek2Tests.m
+//  facebookWeek2Tests
+//
+//  Created by Erica Morse on 6/25/14.
+//  Copyright (c) 2014 YouTube. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface facebookWeek2Tests : XCTestCase
+
+@end
+
+@implementation facebookWeek2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
